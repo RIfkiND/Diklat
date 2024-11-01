0eb294a909d92e9d342a183b5551e202258e4c49
amamFz <ilhamfauzicom21@gmail.com>
2024-11-01 16:16:07 +0700

    fix vite js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -13,8 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      vue: "vue/dist/vue.esm-bundler.js",
-      "@": path.resolve(__dirname, "resources/js/Pages"),
+      "@": path.resolve(__dirname, "resources/js"),
       "~": path.resolve(__dirname, "resources/js"),
     },
   },
