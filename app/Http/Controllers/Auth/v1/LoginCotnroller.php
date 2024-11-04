906bdc2ecd4b0b7ba0c0edbece495e4077fa82be
RIfkiND <122434422+RIfkiND@users.noreply.github.com>
2024-11-04 20:53:00 +0700

    update

---
+++ app/Http/Controllers/Auth/v1/LoginCotnroller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Auth\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class LoginCotnroller extends Controller
+{
+    //
+}
