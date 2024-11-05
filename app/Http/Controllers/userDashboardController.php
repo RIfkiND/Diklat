400648a73ed6b10af6a4bd79096db619566d7ef8
rYmS26 <dyingforme1@gmail.com>
2024-11-05 19:14:10 +0700

    fixed name and routing

--- app/Http/Controllers/userDashboardController.php
+++ app/Http/Controllers/userDashboardController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
-class userDashboardController extends Controller
+class UserDashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Dashboard/User/Index');
+        return Inertia::render('Dashboard/User/index');
     }
 }
