e699edd4fc1718b73c94baf77767985d9e4488cd
Ade Nova <nttnova3@gmail.com>
2024-11-05 07:26:29 +0700

    responsive design for sidebar,monitoring peserta

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,11 +1,17 @@
 import Header from "@/Components/Header";
+import MobileSidebar from "@/Components/MobileSidebar";
 import Sidebar from "@/Components/Sidebar";
 
 export default function AuthenticatedLayout({ children }) {
   return (
     <div className="flex h-screen w-screen overflow-x-hidden">
-      <div className="w-[250px] relative h-full">
-        <Sidebar />
+      <div className="">
+        <div className="hidden md:block md:w-[250px] relative h-full">
+          <Sidebar />
+        </div>
+        <div className="md:hidden absolute h-full z-[99]">
+          <MobileSidebar />
+        </div>
       </div>
 
       <div className="w-full h-full px-5">
