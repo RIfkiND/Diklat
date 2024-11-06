96c34b92ec7b1e2c63d0087526462fe4c4a4864d
rYmS26 <dyingforme1@gmail.com>
2024-11-06 18:49:49 +0700

    pull

--- app/Http/Controllers/userDashboardController.php
+++ app/Http/Controllers/userDashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use App\Models\BiodataPeserta;
 use App\Models\Peserta;
-use App\Http\Requests\Post\V1\Peserta\BiodataPesertaRequest; // Import kelas request
+use App\Http\Requests\Post\V1\Peserta\BiodataPesertaRequest;
 
 class UserDashboardController extends Controller
 {
@@ -18,22 +18,15 @@ class UserDashboardController extends Controller
     {
         $validatedData = $request->validated();
 
-        // Get the current logged-in user
-        $user = auth()->user(); // Get the logged-in user
-
-        // Assuming you have a relationship where each user is linked to a peserta
-        // Let's get the Peserta for the logged-in user
-        $peserta = Peserta::find($user->id); // Assuming user ID matches the peserta ID
+        $user = auth()->user();
+        $peserta = Peserta::find($user->id);
 
         if ($peserta) {
-            // Add the peserta_id to the validated data
             $validatedData['peserta_id'] = $peserta->id;
         } else {
-            // If no peserta found for the logged-in user, return an error
             return redirect()->back()->with('error', 'Peserta not found.');
         }
 
-        // Save the BiodataPeserta record to the database
         try {
             BiodataPeserta::create($validatedData);
             return redirect()->route('user.dashboard')->with('success', 'Biodata berhasil ditambahkan.');
