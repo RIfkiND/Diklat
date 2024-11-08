49a2efc39101384a4854337051f983dbcdc1ad65
Ade Nova <nttnova3@gmail.com>
2024-11-08 15:47:11 +0700

    update finsh edp

--- app/Http/Controllers/Auth/v1/AuthPagesController.php
+++ app/Http/Controllers/Auth/v1/AuthPagesController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Auth\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
 
